4058b5a2663650aa40cf08ece9dfe51dfd951407
acmthinks <acmthinks@users.noreply.github.com>
2016-01-15 13:46:07 -0500

    Update install_sensor.sh

--- install_sensor.sh
+++ install_sensor.sh
@@ -6,25 +6,30 @@ cd ~
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT/
 sudo python setup.py install
-cd examples
-sudo ./AdafruitDHT.py 11 4
+#cd examples
+#sudo ./AdafruitDHT.py 11 4
 
 # Installing Paho Python client for MQTT
-sudo pip install paho-mqtt
 cd ~
+sudo pip install paho-mqtt
 git clone http://git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.python.git
 cd org.eclipse.paho.mqtt.python
 sudo python setup.py install
 
 # Setting sensor data generation script as executable
-cp dht11.py ~
 cd ~
+cp ~/sensor-gateway/dht11.py ~
 chmod 755 dht11.py
 
 # Install Python library for controlling GPIO ports
 sudo pip install RPi.GPIO
 
 # Setting RPi LED script as executable
-cp lightitup.py ~
 cd ~
+cp ~/sensor-gateway/lightitup.py ~
 chmod 755 lightitup.py
+
+# Let's validate that we can get a temp reading and light up the LED
+cd ~/Adafruit_Python_DHT/examples
+sudo ./AdafruitDHT.py 11 4
+sudo python ~/lightitup.py
