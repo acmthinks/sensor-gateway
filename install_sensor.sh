14f98df18cdf8f5ace6a35db19cc6d76ee9f7e6f
acmthinks <acmthinks@users.noreply.github.com>
2016-01-15 13:24:44 -0500

    Update install_sensor.sh

--- install_sensor.sh
+++ install_sensor.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+# Installing DHT11 libraries, courtesy of AdaFruit https://learn.adafruit.com/dht-humidity-sensing-on-raspberry-pi-with-gdocs-logging
 sudo apt-get update
 sudo apt-get install build-essential python-dev python-openssl -y
 cd ~
@@ -7,3 +8,21 @@ cd Adafruit_Python_DHT/
 sudo python setup.py install
 cd examples
 sudo ./AdafruitDHT.py 11 4
+
+# Installing Paho Python client for MQTT
+sudo pip install paho-mqtt
+cd ~
+git clone http://git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.python.git
+cd org.eclipse.paho.mqtt.python
+sudo python setup.py install
+
+# Setting sensor data generation script as executable
+cd ~
+chmod 755 dht11.py
+
+# Install Python library for controlling GPIO ports
+sudo pip install RPi.GPIO
+
+# Setting RPi LED script as executable
+cd ~
+chmod 755 lightitup.py
